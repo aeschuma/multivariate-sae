dff69c8c4428750d695f86b6a303b1640b7b0c66
aeschuma <aeschuma@uw.edu>
2021-07-07 16:17:42 -0700

    testing with more params

--- stage-2-simulation/qsub-stage-2-sims-TEST.sh
+++ stage-2-simulation/qsub-stage-2-sims-TEST.sh
@@ -11,10 +11,10 @@ cd /scratch
 # set local variables
 
 # execute command(s)
-Rscript --no-save --no-restore --verbose /home/students/aeschuma/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel-TEST.R $a $b $c $d $e > stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e.Rout 2>&1
+Rscript --no-save --no-restore --verbose /home/students/aeschuma/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel-TEST.R $a $b $c $d $e $f $g $h $ii $j $k $l $m $n $o $p $q $r $s > stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e_$f_$g_$h_$ii_$j_$k_$l_$m_$n_$o_$p_$q_$r_$s.Rout 2>&1
 
-cp stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e.Rout /home/students/aeschuma/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
-rm -f stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e.Rout
+cp stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e_$f_$g_$h_$ii_$j_$k_$l_$m_$n_$o_$p_$q_$r_$s.Rout /home/students/aeschuma/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
+rm -f stage-2-sim-parallel-TEST_$a_$b_$c_$d_$e_$f_$g_$h_$ii_$j_$k_$l_$m_$n_$o_$p_$q_$r_$s.Rout
 
 rm -f /home/students/aeschuma/qsub-stage-2-sims-TEST.sh*
 rm -f /home/students/aeschuma/s2sim*
