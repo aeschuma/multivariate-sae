b636297ad661413e0c862709ab93f81451ec8d9c
aeschuma <aeschuma@uw.edu>
2021-07-10 14:02:58 -0700

    deleting in the .sh didn't work

--- stage-2-simulation/qsub-stage-2-sims.sh
+++ stage-2-simulation/qsub-stage-2-sims.sh
@@ -16,8 +16,5 @@ Rscript --no-save --no-restore --verbose /home/students/aeschuma/Desktop/survey-
 cp stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$h_$ii_$j_$k_$l_$m_$mm_$n_$o_$p_$q_$r_$rr_$s.Rout /home/students/aeschuma/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
 rm -f stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$h_$ii_$j_$k_$l_$m_$mm_$n_$o_$p_$q_$r_$rr_$s.Rout
 
-rm -f /home/students/aeschuma/qsub-stage-2-sims.sh*
-rm -f /home/students/aeschuma/s2sim*
-
 # notify when script has finished
 # echo "Outfile in /home/students/aeschuma/Dropbox/dissertation_2/survey-csmf/stage-2-simulation/out" | mail -v -s "qsub-stage-2-sims-TEST.sh $a $b $c $d $e $f $g $h $ii $j $k $l $mm $n $o $p $q $r $rr $s $t $u $v $w $x finished" aeschuma@uw.edu
