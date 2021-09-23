5e14a7b3987bf31580bce52ccbc713b14ca33ce5
aeschuma <aeschuma@uw.edu>
2021-09-22 17:35:06 -0700

    total for real run

--- stage-2-simulation/qsub-stage-2-sims.sh
+++ stage-2-simulation/qsub-stage-2-sims.sh
@@ -11,12 +11,12 @@ cd ~/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
 # set local variables
 
 # execute command(s)
-Rscript-R-3.6.1 --no-save --no-restore --verbose ~/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel.R $a $b $c $d $e $f $g $rr $s > stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$rr_$s.Rout 2>&1
+Rscript-R-3.6.1 --no-save --no-restore --verbose ~/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel.R $a $b $c $d $e $f $g $rr $s > stage-2-sim-parallel_$t.Rout 2>&1
 
 # cp stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$rr_$s.Rout ~/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
 # rm -f stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$rr_$s.Rout
 
-rm -f /home/users/aeschuma/s2sim_$a_$b_$c_$d_$e_$f_$g_$rr_$s*
+rm -f /home/users/aeschuma/s2sim_$t*
 rm -f /home/students/aeschuma/qsub-stage-2-sims.sh*
 
 # notify when script has finished
