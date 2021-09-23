8e0854c8feac382ed9f7745820c8236e853c4a97
aeschuma <aeschuma@uw.edu>
2021-09-23 11:00:10 -0700

    compile multiple

--- stage-2-simulation/compile-cluster-sims.R
+++ stage-2-simulation/compile-cluster-sims.R
@@ -53,49 +53,66 @@ resfiles <- grep("results_", files, value = TRUE)
 diagfiles <- grep("standiags_", files, value = TRUE)
 
 ## extract run number to name saved results
-run_number <- as.numeric(regmatches(resfiles[1], regexec("results_run-\\s*(.*?)\\s*_", resfiles[1]))[[1]][2])
-
-# compile results
-results <- readRDS(resfiles[1])
-paramnames <- as.character(results$param)
-measurenames <- names(results)[which(names(results) != "param")]
-results <- array(NA, c(nrow(results), ncol(results) - 1, length(resfiles)))
+run_numbers <- c()
 for (i in 1:length(resfiles)) {
-    results[,,i] <- as.matrix(readRDS(resfiles[i])[, measurenames])
+    run_numbers <- c(run_numbers,
+                     as.numeric(regmatches(resfiles[i], regexec("results_run-\\s*(.*?)\\s*_", resfiles[i]))[[1]][2]))
 }
-results_comp <- cbind(paramnames, as.data.frame(apply(results, c(1, 2), mean)))
-names(results_comp) <- c("param", paste0("mean_",measurenames))
 
-# compile diagnosics
-diags <- readRDS(diagfiles[1])
-stan_diag_names <- names(diags)
-diags <- matrix(NA, nrow = length(diagfiles), ncol = ncol(diags))
-for (i in 1:length(diagfiles)) {
-    diags[i,] <- as.matrix(readRDS(diagfiles[i]))
+run_numbers <- unique(run_numbers)
+results_comp <- diags_comp <- vector(mode = "list", length = length(run_numbers))
+for (rn in 1:length(run_numbers)) {
+    run_number <- run_numbers[rn]
+    
+    # compile results
+    results <- readRDS(resfiles[1])
+    paramnames <- as.character(results$param)
+    measurenames <- names(results)[which(names(results) != "param")]
+    results <- array(NA, c(nrow(results), ncol(results) - 1, length(resfiles)))
+    for (i in 1:length(resfiles)) {
+        results[,,i] <- as.matrix(readRDS(resfiles[i])[, measurenames])
+    }
+    results_comp[[rn]] <- cbind(paramnames, as.data.frame(apply(results, c(1, 2), mean)))
+    names(results_comp[[rn]]) <- c("param", paste0("mean_",measurenames))
+    
+    # compile diagnosics
+    diags <- readRDS(diagfiles[1])
+    stan_diag_names <- names(diags)
+    diags <- matrix(NA, nrow = length(diagfiles), ncol = ncol(diags))
+    for (i in 1:length(diagfiles)) {
+        diags[i,] <- as.matrix(readRDS(diagfiles[i]))
+    }
+    diags_comp[[rn]] <- as.data.frame(t(apply(diags, 2, mean)))
+    names(diags_comp[[rn]]) <- paste0("mean_", stan_diag_names)
+    
+    if (!testing) {
+        # save results
+        setwd(savedir)
+        save(results_comp[[rn]], diags_comp[[rn]], file = paste0("results-diags_run-", run_number,".Rdata"))
+        
+        # move results to the real dropbox if on Box
+        if (root == "P:/") {
+            setwd("C:/Users/aeschuma/Dropbox/dissertation_2/survey-csmf/results/stage-2-simulation")
+            save(results_comp[[rn]], diags_comp[[rn]], file = paste0("results-diags_run-", run_number,".Rdata"))
+        } 
+    }
 }
-diags_comp <- as.data.frame(t(apply(diags, 2, mean)))
-names(diags_comp) <- paste0("mean_", stan_diag_names)
 
 if (!testing) {
-    # save results
-    setwd(savedir)
-    save(results_comp, diags_comp, file = paste0("results-diags_run-", run_number,".Rdata"))
-    
     # delete tmp files
     setwd(paste0(savedir,"/tmp"))
     sapply(c(resfiles, diagfiles), unlink)
     setwd(paste0(savedir,"/../../out/stage-2-simulation"))
     sapply(list.files(), unlink)
     
+    # delete .pe and .po files
     if (root == "P:/") {
-        setwd("C:/Users/aeschuma/Dropbox/dissertation_2/survey-csmf/results/stage-2-simulation")
-        save(results_comp, diags_comp, file = paste0("results-diags_run-", run_number,".Rdata"))
-        
         setwd(root)
-        pe_o_files <- grep("s2sim_", list.files(), value = TRUE)
-        sapply(pe_o_files, unlink)
+        pe_po_files <- grep("s2sim_", list.files(), value = TRUE)
+        sapply(pe_po_files, unlink)
     } else if (root == "/home/users/aeschuma/") {
         setwd(root)
         system("rm -f /home/users/aeschuma/s2sim_*")
     }
 }
+
