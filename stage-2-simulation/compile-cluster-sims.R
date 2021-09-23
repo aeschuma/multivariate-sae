4b48794ed577ae71b88e97b8edfee359a3832fd5
aeschuma <aeschuma@uw.edu>
2021-09-23 11:20:13 -0700

    compile code update

--- stage-2-simulation/compile-cluster-sims.R
+++ stage-2-simulation/compile-cluster-sims.R
@@ -19,22 +19,11 @@ if (root == "P:/") {
     .libPaths(paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"))
 }
 
-## load libraries
-if (root == "/home/users/aeschuma/") {
-    library(Rcpp,lib.loc=paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"));
-    library(StanHeaders,lib.loc=paste0(root,"R/x86_64-pc-linux-gnu-library/3.6")); 
-    library(BH,lib.loc = paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"))
-    library(rstan,lib.loc = paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"));
-} else {
-    library(Rcpp); library(StanHeaders); library(BH); library(rstan);library(bayesplot);
-}
-library(scales); library(RColorBrewer); library(ggplot2); 
-
 ########
 ## TESTING THE CODE?
 ########
 
-testing <- TRUE
+testing <- FALSE
 
 ## define directories
 
@@ -60,40 +49,45 @@ for (i in 1:length(resfiles)) {
 }
 
 run_numbers <- unique(run_numbers)
-results_comp <- diags_comp <- vector(mode = "list", length = length(run_numbers))
 for (rn in 1:length(run_numbers)) {
+    setwd(paste0(savedir,"/tmp"))
+    
     run_number <- run_numbers[rn]
     
+    tmp.resfiles <- grep(paste0("results_run-", run_number), resfiles, value = TRUE)
+    tmp.diagfiles <- grep(paste0("standiags_run-", run_number), diagfiles, value = TRUE)
+    
     # compile results
-    results <- readRDS(resfiles[1])
+    results <- readRDS(tmp.resfiles[1])
     paramnames <- as.character(results$param)
     measurenames <- names(results)[which(names(results) != "param")]
-    results <- array(NA, c(nrow(results), ncol(results) - 1, length(resfiles)))
-    for (i in 1:length(resfiles)) {
-        results[,,i] <- as.matrix(readRDS(resfiles[i])[, measurenames])
+    results <- array(NA, c(nrow(results), ncol(results) - 1, length(tmp.resfiles)))
+    for (i in 1:length(tmp.resfiles)) {
+        results[,,i] <- as.matrix(readRDS(tmp.resfiles[i])[, measurenames])
     }
-    results_comp[[rn]] <- cbind(paramnames, as.data.frame(apply(results, c(1, 2), mean)))
-    names(results_comp[[rn]]) <- c("param", paste0("mean_",measurenames))
+    results_comp <- cbind(paramnames, as.data.frame(apply(results, c(1, 2), mean)))
+    names(results_comp) <- c("param", paste0("mean_",measurenames))
     
     # compile diagnosics
-    diags <- readRDS(diagfiles[1])
+    diags <- readRDS(tmp.diagfiles[1])
     stan_diag_names <- names(diags)
-    diags <- matrix(NA, nrow = length(diagfiles), ncol = ncol(diags))
-    for (i in 1:length(diagfiles)) {
-        diags[i,] <- as.matrix(readRDS(diagfiles[i]))
+    diags <- matrix(NA, nrow = length(tmp.diagfiles), ncol = ncol(diags))
+    for (i in 1:length(tmp.diagfiles)) {
+        diags[i,] <- as.matrix(readRDS(tmp.diagfiles[i]))
     }
-    diags_comp[[rn]] <- as.data.frame(t(apply(diags, 2, mean)))
-    names(diags_comp[[rn]]) <- paste0("mean_", stan_diag_names)
+    diags_comp <- as.data.frame(t(apply(diags, 2, mean)))
+    names(diags_comp) <- paste0("mean_", stan_diag_names)
     
     if (!testing) {
+        
         # save results
         setwd(savedir)
-        save(results_comp[[rn]], diags_comp[[rn]], file = paste0("results-diags_run-", run_number,".Rdata"))
+        save(results_comp, diags_comp, file = paste0("results-diags_run-", run_number,".Rdata"))
         
         # move results to the real dropbox if on Box
         if (root == "P:/") {
             setwd("C:/Users/aeschuma/Dropbox/dissertation_2/survey-csmf/results/stage-2-simulation")
-            save(results_comp[[rn]], diags_comp[[rn]], file = paste0("results-diags_run-", run_number,".Rdata"))
+            save(results_comp, diags_comp, file = paste0("results-diags_run-", run_number,".Rdata"))
         } 
     }
 }
