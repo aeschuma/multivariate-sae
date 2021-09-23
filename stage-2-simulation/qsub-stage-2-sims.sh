2e0af9844b1f0125331e5277130f42205c342989
aeschuma <aeschuma@uw.edu>
2021-09-22 17:38:27 -0700

    no more email

--- stage-2-simulation/qsub-stage-2-sims.sh
+++ stage-2-simulation/qsub-stage-2-sims.sh
@@ -20,4 +20,4 @@ rm -f /home/users/aeschuma/s2sim_$t*
 rm -f /home/students/aeschuma/qsub-stage-2-sims.sh*
 
 # notify when script has finished
-echo "Outfile in /home/users/aeschuma/Dropbox/dissertation_2/survey-csmf/stage-2-simulation/out" | mail -v -s "qsub-stage-2-sims-TEST.sh $a $b $c $d $e $f $g $rr $s finished" aeschuma@uw.edu
+# echo "Outfile in /home/users/aeschuma/Dropbox/dissertation_2/survey-csmf/stage-2-simulation/out" | mail -v -s "qsub-stage-2-sims-TEST.sh $a $b $c $d $e $f $g $rr $s finished" aeschuma@uw.edu
