1ad2a46ca975e464c957b708b9c536d82d858d00
aeschuma <aeschuma@uw.edu>
2021-09-29 12:20:49 -0700

    try again

--- stage-2-simulation/loop_submit_stage-2-sims.R
+++ stage-2-simulation/loop_submit_stage-2-sims.R
@@ -47,18 +47,18 @@ setwd(wd)
 ## which model and dgm to run?
 # see model-info.csv
 model_to_run <- c(1, 2)
-# dgm_to_run <- c(1, 2)
-dgm_to_run <- 1:12
+dgm_to_run <- c(1, 2)
+# dgm_to_run <- 1:12
 
 ## STAN options
 niter <- 8000
 nchains <- 2
 prop_warmup <- 0.5
-max_treedepth <- 25
-adapt_delta <- 0.8
+max_treedepth <- 50
+adapt_delta <- 0.85
 
 ## Simulation number
-number_of_sims <- 3
+number_of_sims <- 2
 
 ## loop and submit jobs
 running_total <- 0
