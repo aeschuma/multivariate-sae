48b6600c5e60ae9d378383c5e5900f9f7ee30601
aeschuma <aeschuma@uw.edu>
2022-03-03 23:16:39 -0800

    try again

--- stage-2-simulation/qsub-stage-2-sims.sh
+++ stage-2-simulation/qsub-stage-2-sims.sh
@@ -11,7 +11,7 @@ cd ~/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
 # set local variables
 
 # execute command(s)
-Rscript-R-3.6.1 --no-save --no-restore --verbose ~/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel.R $a $rr $s > stage-2-sim-parallel_$t.Rout 2>&1
+Rscript --no-save --no-restore --verbose ~/Desktop/survey-csmf/stage-2-simulation/stage-2-sim-parallel.R $a $rr $s > stage-2-sim-parallel_$t.Rout 2>&1
 
 # cp stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$rr_$s.Rout ~/Dropbox/dissertation_2/survey-csmf/out/stage-2-simulation
 # rm -f stage-2-sim-parallel_$a_$b_$c_$d_$e_$f_$g_$rr_$s.Rout
--- stage-2-simulation/stage-2-sim-parallel.R
+++ stage-2-simulation/stage-2-sim-parallel.R
@@ -8,19 +8,6 @@ root <- ifelse(Sys.info()[1]=="Darwin","~/",
                       ifelse(Sys.info()[1]=="Linux","/home/users/aeschuma/",
                              stop("Unknown operating system"))))
 
-## the following code makes rstan work on the Box server and the cluster
-if (root == "P:/") {
-    Sys.setenv(HOME="C:/Users/aeschuma",
-               R_USER="C:/Users/aeschuma",
-               R_LIBS_USER="C:/Users/aeschuma/R_libraries")
-    .libPaths("C:/Users/aeschuma/R_libraries")
-} else if (root == "/home/users/aeschuma/") {
-    Sys.setenv(HOME=root,
-               R_USER=root,
-               R_LIBS_USER=paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"))
-    .libPaths(paste0(root,"R/x86_64-pc-linux-gnu-library/3.6"))
-}
-
 ## load libraries
 library(mvtnorm); library(MASS);library(gtools); library(parallel);
 library(scales); library(RColorBrewer); library(ggplot2); library(tidyverse); library(dplyr);
